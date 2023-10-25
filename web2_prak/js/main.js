a5a88b220a0408c6ebdc43ee583f8195c51e9758
Asmodeus115 <ak.work115@gmail.com>
2023-10-25 17:29:16 +0200

    Grunderüst V1.1

---
+++ web2_prak/js/main.js
@@ -0,0 +1,9 @@
+const kalenderTabId = document.getElementsByClassName(); // classennamen des elemtns eintrage
+const kalenderTabClass = document.getElementById(); //Id des elements eintrage
+// funktion zur farbänderung
+function farbeAendern(){
+  meinElement.style.color = 'rot'
+}
+
+//Eventlistener
+meinElement.addEventListener('', farbeAendern); //hier -> '' ,muss man noch eintragen was passiert, z.B. 'click' oder so
