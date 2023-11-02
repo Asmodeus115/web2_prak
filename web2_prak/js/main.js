b5ecbefd56fe1d87a099f70a84d922b5513b8af3
Asmodeus115 <ak.work115@gmail.com>
2023-11-02 18:32:51 +0100

    Grunderüst V1.5

--- web2_prak/js/main.js
+++ web2_prak/js/main.js
@@ -1,9 +1,22 @@
-const kalenderTabId = document.getElementsByClassName(); // classennamen des elemtns eintrage
-const kalenderTabClass = document.getElementById(); //Id des elements eintrage
+const kalenderTabId = document.getElementsByClassName(""); // classennamen des elemtns eintrage
+const kalenderTabClass = document.getElementById(""); //Id des elements eintrage
+const loadingInfoId = document.getElementById("loading-info");
+
 // funktion zur farbänderung
 function farbeAendern(){
-  meinElement.style.color = 'rot'
+ //meinElement.style.color = 'rot'
 }
-
 //Eventlistener
-meinElement.addEventListener('', farbeAendern); //hier -> '' ,muss man noch eintragen was passiert, z.B. 'click' oder so
+//meinElement.addEventListener('', farbeAendern); //hier -> '' ,muss man noch eintragen was passiert, z.B. 'click' oder so
+
+/*
+function domLoad(){
+  console.log("HELLOOOOOOO!")
+  let loading = document.getElementById("loading-info");
+  loading.remove();
+  initApp();
+}
+*/
+function initApp(){
+  console.log("app geladen")
+}
