403d173009efa72761f98d847d7abe96baac4d07
Asmodeus115 <ak.work115@gmail.com>
2023-11-30 18:43:44 +0100

    hilfe

--- ReserveIT_Website/Frontend/js/main.js
+++ ReserveIT_Website/Frontend/js/main.js
@@ -1,85 +1,55 @@
 
-/*
-  if (document.querySelectorAll(".linkleisteMain").length>0){
-    document.querySelectorAll(".linkleisteMain").forEach(link =>{
-      link.addEventListener("click", (event)=>{
-
-        if () {
-
-        } else if (*event.target.className === "kalenderBtn") {
-
-        } else if (event.target.className === "datenschutzBtn") {
-
-        } else if (event.target.className === "impressumBtn") {
-
-        } else if (event.target.className === "buchungenBtn") {
-
-        } else if (event.target.className === "ueberunsBtn") {
-
-        } else if (event.target.className === "kontaktBtn") {
-
-        } else if (event.target.className === "anfahrtBtn") {
-
-        }else {
-          console.log("fehler linkleiste")
-        }
-      });
-    });
-}
-
-  const links206 = document.querySelectorAll(".btn206");
-  const links201 = document.querySelectorAll(".btn201");
-  const links210 = document.querySelectorAll(".btn210");
-  const links205 = document.querySelectorAll(".btn205");
-
-  if (links201.length>0){
-    links201.forEach(link =>{
-      link.addEventListener("click",() =>{
-        document.createElement('')
-      });
-    });
-  }
-  if (links206.length>0){
-    links206.forEach(link =>{
-      link.addEventListener("click",() =>{
-
-      });
-    });
-  }
-  if (links205.length>0){
-    links205.forEach(link =>{
-      link.addEventListener("click",() =>{
-
-        console.log("oha1")
-      });
-    });
-  }
-  if (links210.length>0){
-    links210.forEach(link =>{
-      link.addEventListener("click",() =>{
-
-      });
-    });
-
-
-}
-*/
 function loadSVG(url){
     fetch(url)
         .then(response => response.text())
         .then(svgData => {
             document.getElementById('lageplan').innerHTML = svgData;
-
-            document.getElementById('logo').addEventListener('click',()=>{
-                console.log('click');
-            });
         })
         .catch(error =>{
-            console.log("clickfail", error);
+            console.log("loading fail", error);
         })
 }
 
-function calendar(){
+function swapButtonsGeb(gebNr){
+    loadSVG('http://localhost:3000/img/'+gebNr+"_"+1+".svg");
+    etagenAdder(gebNr);
+  }
 
+  function etagenWechsel(etagenNr){
+
+  }
+
+function etagenAdder(gebNr){
+    const etagen = document.getElementById("gebBtn");
+    let i = 1;
+    const btn = etagen.querySelectorAll('.btn');
+    btn.forEach( function (btn) {
+        btn.setAttribute('id',gebNr+"_"+i);
+        switch (i){
+            case 1:{
+                btn.textContent = gebNr+" "+"EG";
+                break;
+            }
+            case 2:{
+                btn.textContent = gebNr+" "+"OG 1";
+                break;
+            }
+            case 3:{
+                btn.textContent = gebNr+" "+"OG 2";
+                break;
+            }
+            case 4:{
+                btn.textContent = gebNr+" "+"OG 3";
+                break;
+            }
+            case 5:{
+                btn.textContent = gebNr+" "+"OG 4";
+                break;
+            }
+        }
+        i++;
+    });
 }
+
+
 loadSVG('http://localhost:3000/img/campusplan_raeume_albstadt.svg');
