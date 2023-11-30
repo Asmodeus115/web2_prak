9be5d0c2b8b3725601d0ba8616311186d7bb2d47
Asmodeus115 <ak.work115@gmail.com>
2023-11-30 14:53:41 +0100

    hilfe

--- ReserveIT_Website/Frontend/js/main.js
+++ ReserveIT_Website/Frontend/js/main.js
@@ -1,32 +1,32 @@
 
+/*
+  if (document.querySelectorAll(".linkleisteMain").length>0){
+    document.querySelectorAll(".linkleisteMain").forEach(link =>{
+      link.addEventListener("click", (event)=>{
 
-/*linkleiste index.html*/
-if(window.location.pathname.includes("index.html")){
-  document.getElementById("linkleisteMain").addEventListener("click", function (event){
-    if (event.target.id === "lageplanBtn") {
-      window.location.href = "index.html";
-    } else if (event.target.id === "kalenderBtn") {
-      window.location.href = "Calendar.html";
-    } else if (event.target.id === "datenschutzBtn") {
-      window.location.href = "Datenschutz.html";
-    } else if (event.target.id === "impressumBtn") {
-      window.location.href = "Impressum.html";
-    } else if (event.target.id === "buchungenBtn") {
-      window.location.href = "Calendar.html";
-    } else if (event.target.id === "ueberunsBtn") {
-      window.location.href = "ueberuns.html";
-    } else if (event.target.id === "kontaktBtn") {
-      window.location.href = "kontakt.html"; 
-    } else if (event.target.id === "meineBuchungenBtn") {
-      window.location.href = "#";
-    } else if (event.target.id === "anfahrtBtn") {
-      window.location.href = "https://maps.app.goo.gl/uSkcTxGHvDX1AJY8A";
-    }else {
-      console.log("fehler linkleiste")
-    }
-  });
+        if () {
+
+        } else if (*event.target.className === "kalenderBtn") {
+
+        } else if (event.target.className === "datenschutzBtn") {
+
+        } else if (event.target.className === "impressumBtn") {
+
+        } else if (event.target.className === "buchungenBtn") {
+
+        } else if (event.target.className === "ueberunsBtn") {
+
+        } else if (event.target.className === "kontaktBtn") {
+
+        } else if (event.target.className === "anfahrtBtn") {
+
+        }else {
+          console.log("fehler linkleiste")
+        }
+      });
+    });
 }
-  /*linkleiste gebaeude*/
+
   const links206 = document.querySelectorAll(".btn206");
   const links201 = document.querySelectorAll(".btn201");
   const links210 = document.querySelectorAll(".btn210");
@@ -35,21 +35,21 @@ if(window.location.pathname.includes("index.html")){
   if (links201.length>0){
     links201.forEach(link =>{
       link.addEventListener("click",() =>{
-        window.location.href="geb_201.html";
+        document.createElement('')
       });
     });
   }
   if (links206.length>0){
     links206.forEach(link =>{
       link.addEventListener("click",() =>{
-        window.location.href="geb_206.html";
+
       });
     });
   }
   if (links205.length>0){
     links205.forEach(link =>{
       link.addEventListener("click",() =>{
-        window.location.href="geb_205.html";
+
         console.log("oha1")
       });
     });
@@ -57,21 +57,29 @@ if(window.location.pathname.includes("index.html")){
   if (links210.length>0){
     links210.forEach(link =>{
       link.addEventListener("click",() =>{
-        window.location.href="geb_210.html";
+
       });
     });
-  /*sidebarButtons gebaeude*/
+
 
 }
-  const logos = document.querySelectorAll(".logo");
+*/
+function loadSVG(url){
+    fetch(url)
+        .then(response => response.text())
+        .then(svgData => {
+            document.getElementById('lageplan').innerHTML = svgData;
 
-  if (logos.length>0){
-    logos.forEach(logo =>{
-      logo.addEventListener('click', () =>{
-        window.location.href="index.html";
-      });
-    });
-  }else {
-    console.log("fehler logo")
-  }
+            document.getElementById('logo').addEventListener('click',()=>{
+                console.log('click');
+            });
+        })
+        .catch(error =>{
+            console.log("clickfail", error);
+        })
+}
 
+function calendar(){
+
+}
+loadSVG('http://localhost:3000/img/campusplan_raeume_albstadt.svg');
