1b27e0ee1195cda7c94d55cb273d6a5c5259bcae
userNr0 <ahmad.alhussein.office@gmail.com>
2023-12-01 14:10:50 +0100

    gsggsg

--- ReserveIT_Website/Frontend/js/connections.js
+++ ReserveIT_Website/Frontend/js/connections.js
@@ -4,7 +4,7 @@
 // Add a submit event listener to the form
 $('#signInBtn').submit(function (event) {
 
-    var matrikelNr =  document.getElementById('login__matrikelnr').value;
+    var matrikelNr = document.getElementById('login__matrikelnr').value;
     console.log("LogIn Vorgang startet.");
     // Get the login form by ID
     // const loginForm = document.querySelector('.form');
@@ -175,7 +175,7 @@ $('#bookModal').submit(function (event) {
         return result;
     }
 
-    
+
     var datum = $('#bookDate').val();
     var start = $('#bookStart').val();
     var end = $('#bookEnd').val();
@@ -245,7 +245,7 @@ $('#lagerplan').click(function (event) {
     console.log("zeige Gebäude ");
 
     // disable default event
-    //event.preventDefault();
+    event.preventDefault();
 
     function zeigeBuchungen(arr) {
         var tmp;
@@ -255,18 +255,19 @@ $('#lagerplan').click(function (event) {
             return;
         }
 
+        tmp += '<div></div>'
         tmp += '<ul class="list-group" id="gebBtn">';
 
         console.log("log test");
         var i = 1;
         arr.forEach(obj => {
-            tmp += '<li id="GebBtn'+ obj.Name + '" class="list-group-item btn btn'+ obj.Name +'" onclick="swapButtonsGeb(\''+ obj.Name + '\')">' + obj.Name + '</li>';
-            console.log( '<li id="GebBtn'+ obj.Name + '" class="list-group-item btn btn'+ obj.Name +'"onclick="swapButtonsGeb(\''+ obj.Name + '\')">' + obj.Name + '</li>');
+            tmp += '<li id="GebBtn' + obj.Name + '" class="list-group-item btn btn' + obj.Name + '" onclick="swapButtonsGeb(\'' + obj.Name + '\')">' + obj.Name + '</li>';
+            console.log('<li id="GebBtn' + obj.Name + '" class="list-group-item btn btn' + obj.Name + '"onclick="swapButtonsGeb(\'' + obj.Name + '\')">' + obj.Name + '</li>');
         });
 
-        tmp+='</ul>';
+        tmp += '</ul>';
 
-        $('#GebaeudeListe').html(tmp);
+        $('#tabelle').html(tmp);
 
     }
 
@@ -288,36 +289,116 @@ $('#lagerplan').click(function (event) {
         console.log('response received');
         console.log(response);
         zeigeBuchungen(response);
-        ladeGrundriss();
+        ladeGrundriss(response);
 
     }).fail(function (xhr) {
-        console.log('error received');
     });
 
 
 
-    function ladeGrundriss(){
+    function ladeGrundriss(response) {
 
         $(document).ready(function () {
             // IDs der <li>-Elemente im #gebBtn-Element abrufen
-            var liIds = $('#gebBtn li').map(function() {
+            var liIds = $('#gebBtn li').map(function () {
                 return this.id;
             }).get();
-        
+
             // Jetzt kannst du auf die IDs zugreifen oder sie ausgeben
             console.log(liIds);
-        
+
             // Beispiel: Iteration über die IDs
-            liIds.forEach(function(id) {
+            liIds.forEach(function (id) {
                 console.log("ID: " + id);
+
+
+                $('#'+id).click(function (event) {
+
+                    console.log(response);
+
+                    // disable default event
+                    event.preventDefault();
+                    //$('#lageplan').hide();
+
+                    /*
+                        function zeigeBuchungen(arr) {
+                    
+                            $('#titel').html('<h1>Mein Buchungen</h1>');
+                            var tmp;
+                    
+                            if (arr.length == 0) {
+                                tmp.text('Keine Buchung vorhanden');
+                                return;
+                            }
+                    
+                    
+                            tmp += '<tr>';
+                            tmp += '<th>Nr</th>';
+                            tmp += '<th>RaumID</th>';
+                            tmp += '<th>BenutzerID</th>';
+                            tmp += '<th>Startzeit</th>';
+                            tmp += '<th>Endzeit</th>';
+                            tmp += '<th>BuchungCode</th>';
+                            tmp += '</tr>';
+                    
+                            console.log("log test");
+                            var i = 1;
+                            arr.forEach(obj => {
+                                tmp += '<tr>';
+                                tmp += '<td>' + i + '</td>';
+                                tmp += '<td>' + obj.RaumID + '</td>';
+                                tmp += '<td>' + obj.BenutzerID + '</td>';
+                                tmp += '<td>' + obj.Startzeit + '</td>';
+                                tmp += '<td>' + obj.Endzeit + '</td>';
+                                tmp += '<td>' + obj.BuchungCode + '</td>';
+                                //tmp += '<td>' + (obj.alter >= 18 ? 'erwachsen' : 'Kind') + '</td>';
+                                tmp += '</tr>';
+                                i++;
+                            });
+                    
+                            $('#tabelle').html(tmp);
+                    
+                        }
+                    
+                    */
+
+                    console.log('loading all recs from api');
+                    // convert data of form to object
+                    var meinObjekt = {
+                        id: 210
+                    };
+
+                    // Erstellen Sie ein neues FormData-Objekt
+                    var formData = new FormData();
+
+                    // Fügen Sie jedes Element aus dem JSON-Objekt zum FormData-Objekt hinzu
+                    for (var schluessel in meinObjekt) {
+                        formData.append(schluessel, meinObjekt[schluessel]);
+                    }
+
+                    console.log(formData);
+
+                    // send form with ajax
+                    $.ajax({
+                        url: 'http://localhost:8000/api/etage/laden',
+                        type: 'POST',
+                        data: formData,
+                        contentType: false,
+                        cache: false,
+                        processData: false,
+                        dataType: 'json'
+                    }).done(function (response) {
+                        console.log('response from Etage received');
+                        console.log(response);
+                        //zeigeBuchungen(response);
+
+                    }).fail(function (xhr) {
+                        console.log('error received');
+                    });
+                });
             });
         });
-
-
-
     };
-
-
 });
 
 
@@ -335,52 +416,52 @@ $('#GebBtn210').click(function (event) {
     event.preventDefault();
     //$('#lageplan').hide();
 
-/*
-    function zeigeBuchungen(arr) {
-
-        $('#titel').html('<h1>Mein Buchungen</h1>');
-        var tmp;
-
-        if (arr.length == 0) {
-            tmp.text('Keine Buchung vorhanden');
-            return;
-        }
-
-
-        tmp += '<tr>';
-        tmp += '<th>Nr</th>';
-        tmp += '<th>RaumID</th>';
-        tmp += '<th>BenutzerID</th>';
-        tmp += '<th>Startzeit</th>';
-        tmp += '<th>Endzeit</th>';
-        tmp += '<th>BuchungCode</th>';
-        tmp += '</tr>';
-
-        console.log("log test");
-        var i = 1;
-        arr.forEach(obj => {
+    /*
+        function zeigeBuchungen(arr) {
+    
+            $('#titel').html('<h1>Mein Buchungen</h1>');
+            var tmp;
+    
+            if (arr.length == 0) {
+                tmp.text('Keine Buchung vorhanden');
+                return;
+            }
+    
+    
             tmp += '<tr>';
-            tmp += '<td>' + i + '</td>';
-            tmp += '<td>' + obj.RaumID + '</td>';
-            tmp += '<td>' + obj.BenutzerID + '</td>';
-            tmp += '<td>' + obj.Startzeit + '</td>';
-            tmp += '<td>' + obj.Endzeit + '</td>';
-            tmp += '<td>' + obj.BuchungCode + '</td>';
-            //tmp += '<td>' + (obj.alter >= 18 ? 'erwachsen' : 'Kind') + '</td>';
+            tmp += '<th>Nr</th>';
+            tmp += '<th>RaumID</th>';
+            tmp += '<th>BenutzerID</th>';
+            tmp += '<th>Startzeit</th>';
+            tmp += '<th>Endzeit</th>';
+            tmp += '<th>BuchungCode</th>';
             tmp += '</tr>';
-            i++;
-        });
-
-        $('#tabelle').html(tmp);
-
-    }
-
-*/
+    
+            console.log("log test");
+            var i = 1;
+            arr.forEach(obj => {
+                tmp += '<tr>';
+                tmp += '<td>' + i + '</td>';
+                tmp += '<td>' + obj.RaumID + '</td>';
+                tmp += '<td>' + obj.BenutzerID + '</td>';
+                tmp += '<td>' + obj.Startzeit + '</td>';
+                tmp += '<td>' + obj.Endzeit + '</td>';
+                tmp += '<td>' + obj.BuchungCode + '</td>';
+                //tmp += '<td>' + (obj.alter >= 18 ? 'erwachsen' : 'Kind') + '</td>';
+                tmp += '</tr>';
+                i++;
+            });
+    
+            $('#tabelle').html(tmp);
+    
+        }
+    
+    */
 
     console.log('loading all recs from api');
     // convert data of form to object
     var meinObjekt = {
-        id:2
+        id: 2
     };
 
     // Erstellen Sie ein neues FormData-Objekt
