70e5227c1f626e29b0c6f15e79e2bd9ee1a8ec40
userNr0 <ahmad.alhussein.office@gmail.com>
2024-01-10 18:23:23 +0100

    unnötige DB files gelöscht

---
+++ ReserveIT_Website/Frontend/js/setupWebsite.js
@@ -0,0 +1,16 @@
+
+// Beim Aufrufen der Webseite sollen folgende Funktionen ausgeführt.
+
+
+$(document).ready(function () {
+
+    setupLogin();
+    ladeImpressum();
+    ladeAboutUS();
+    ladeFooter();
+
+
+
+
+
+});
