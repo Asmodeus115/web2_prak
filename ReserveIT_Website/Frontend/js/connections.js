470c749c72fa4235098eaa30505f00b6672b7b14
Asmodeus115 <ak.work115@gmail.com>
2024-01-11 16:15:29 +0100

    test

--- ReserveIT_Website/Frontend/js/connections.js
+++ ReserveIT_Website/Frontend/js/connections.js
@@ -1,10 +1,11 @@
-
-
+loginProcess();
 // Diese Funktionen sind für den Anmelde-Vorgang
 // Sie prüft, ob der User schon existiert. Falls ja,
 // wird der User an die index.html weitergeleitet.
 
-$('#signInBtn').submit(function (event) {
+
+function loginProcess(){
+  $('#signInBtn').submit(function (event) {
 
     const matrikelNr = document.getElementById('login__matrikelnr').value;
     console.log("LogIn Vorgang startet.");
@@ -15,8 +16,8 @@ $('#signInBtn').submit(function (event) {
 
     // convert data of form to object
     const meinObjekt = {
-        id: matrikelNr,
-        passwort: password,
+      id: matrikelNr,
+      passwort: password,
     };
 
     // Erstellen Sie ein neues FormData-Objekt
@@ -24,47 +25,51 @@ $('#signInBtn').submit(function (event) {
 
     // Fügen Sie jedes Element aus dem JSON-Objekt zum FormData-Objekt hinzu
     for (const schluessel in meinObjekt) {
-        formData.append(schluessel, meinObjekt[schluessel]);
+      formData.append(schluessel, meinObjekt[schluessel]);
     }
 
     console.log(formData);
 
     // send form with ajax
     $.ajax({
-        url: 'http://localhost:8000/api/benutzer/existiert',
-        type: 'POST',
-        data: formData,
-        contentType: false,
-        cache: false,
-        processData: false,
-        dataType: 'json'
+      url: 'http://localhost:8000/api/benutzer/existiert',
+      type: 'POST',
+      data: formData,
+      contentType: false,
+      cache: false,
+      processData: false,
+      dataType: 'json'
     }).done(function (response) {
-        console.log('Anmeldung = ' + response);
+      console.log('Anmeldung = ' + response);
 
-        if (response) {
-            console.log('Anmeldung removed');
-            //$('.grid-layout').insertBefore('<h1>htest</h1>');
+      if (response) {
+        console.log('Anmeldung removed');
+        //$('.grid-layout').insertBefore('<h1>htest</h1>');
 
-            $('#loginArea').empty();
-            //$('#loginArea').insertAfter('<main id="grid-unten"><h1>htest</h1></main>');
-            //ladeWebseite();
-            loginKiller();
-            runMain();
+        $('#loginArea').empty();
+        //$('#loginArea').insertAfter('<main id="grid-unten"><h1>htest</h1></main>');
+        //ladeWebseite();
+        signedInLinkBar();
+        loginKiller();
+        runMain();
+        meineBuchungen();
 
-        } else {
-            $('#fehler').html('Fehler beim Anmelden');
-            $('#login__matrikelnr').val('');
-            $('#login__password').val('');
-        }
-
-    }).fail(function (xhr) {
-        console.log('error received');
+      } else {
+        $('#fehler').html('Fehler beim Anmelden');
         $('#login__matrikelnr').val('');
         $('#login__password').val('');
-        $('#fehler').html('Fehler beim Anmelden');
+      }
+
+    }).fail(function (xhr) {
+      console.log('error received');
+      $('#login__matrikelnr').val('');
+      $('#login__password').val('');
+      $('#fehler').html('Fehler beim Anmelden');
     });
 
-});
+  });
+}
+
 
 
 function ladeWebseite() {
@@ -82,7 +87,8 @@ function ladeWebseite() {
 // Meine Buchungen geklick wird. Es werden alle
 // Buchungen aufgelistet, die der User hat.
 
-$('#meineBuchungenBtn').click(function (event) {
+function meineBuchungen() {
+  $('#meineBuchungenBtn').click(function (event) {
 
     $('#grid-unten').empty();
 
@@ -91,46 +97,46 @@ $('#meineBuchungenBtn').click(function (event) {
 
 
     function zeigeBuchungen(arr) {
-        $('#grid-unten').empty();
+      $('#grid-unten').empty();
 
-        var tmp = "1";
+      var tmp = "1";
 
-        if (arr.length === 0) {
-            tmp.text('Keine Buchung vorhanden');
-            return;
-        }
+      if (arr.length === 0) {
+        tmp.text('Keine Buchung vorhanden');
+        return;
+      }
 
-        tmp += '<table id="tabelle"><tr>';
-        tmp += '<th>Nr</th>';
-        tmp += '<th>RaumID</th>';
-        tmp += '<th>BenutzerID</th>';
-        tmp += '<th>Startzeit</th>';
-        tmp += '<th>Endzeit</th>';
-        tmp += '<th>BuchungCode</th>';
-        tmp += '<th>Stornieren</th>';
+      tmp += '<table id="tabelle"><tr>';
+      tmp += '<th>Nr</th>';
+      tmp += '<th>RaumID</th>';
+      tmp += '<th>BenutzerID</th>';
+      tmp += '<th>Startzeit</th>';
+      tmp += '<th>Endzeit</th>';
+      tmp += '<th>BuchungCode</th>';
+      tmp += '<th>Stornieren</th>';
+      tmp += '</tr>';
+
+      var i = 1;
+      arr.forEach(obj => {
+        tmp += '<tr>';
+        tmp += '<td>' + i + '</td>';
+        tmp += '<td>' + obj.RaumID + '</td>';
+        tmp += '<td>' + obj.BenutzerID + '</td>';
+        tmp += '<td>' + obj.Startzeit + '</td>';
+        tmp += '<td>' + obj.Endzeit + '</td>';
+        tmp += '<td>' + obj.BuchungCode + '</td>';
+        tmp += '<td><input class="storinerenBtn" type="checkbox" id=' + obj.id + '> Stornieren </td>';
         tmp += '</tr>';
-
-        var i = 1;
-        arr.forEach(obj => {
-            tmp += '<tr>';
-            tmp += '<td>' + i + '</td>';
-            tmp += '<td>' + obj.RaumID + '</td>';
-            tmp += '<td>' + obj.BenutzerID + '</td>';
-            tmp += '<td>' + obj.Startzeit + '</td>';
-            tmp += '<td>' + obj.Endzeit + '</td>';
-            tmp += '<td>' + obj.BuchungCode + '</td>';
-            tmp += '<td><input class="storinerenBtn" type="checkbox" id=' + obj.id + '> Stornieren </td>';
-            tmp += '</tr>';
-            i++;
-        });
-        tmp += '</table>';
-        $('#grid-unten').append(tmp);
+        i++;
+      });
+      tmp += '</table>';
+      $('#grid-unten').append(tmp);
     }
 
     // convert data of form to object
     const meinObjekt = {
-        //BenutzerID: sessionStorage.getItem('MatrikelNr')
-        BenutzerID: 12345
+      //BenutzerID: sessionStorage.getItem('MatrikelNr')
+      BenutzerID: 12345
     };
 
     // Erstellen ein neues FormData-Objekt
@@ -138,72 +144,72 @@ $('#meineBuchungenBtn').click(function (event) {
 
     // Fügen Sie jedes Element aus dem JSON-Objekt zum FormData-Objekt hinzu
     for (const schluessel in meinObjekt) {
-        formData.append(schluessel, meinObjekt[schluessel]);
+      formData.append(schluessel, meinObjekt[schluessel]);
     }
 
     // send form with ajax
     $.ajax({
-        url: 'http://localhost:8000/api/buchung/ladeMeineBuchungen',
-        type: 'POST',
-        data: formData,
-        contentType: false,
-        cache: false,
-        processData: false,
-        dataType: 'json'
+      url: 'http://localhost:8000/api/buchung/ladeMeineBuchungen',
+      type: 'POST',
+      data: formData,
+      contentType: false,
+      cache: false,
+      processData: false,
+      dataType: 'json'
     }).done(function (response) {
-        console.log('response received');
-        console.log(response);
-        zeigeBuchungen(response);
+      console.log('response received');
+      console.log(response);
+      zeigeBuchungen(response);
 
-        var SlotsToCancle = [];
-        var showBtn = 0;
+      var SlotsToCancle = [];
+      var showBtn = 0;
 
-        var checkbox = document.getElementsByClassName('storinerenBtn');
-        $('#grid-unten').append('<button  id="conformStorno" type="submit" value="Stornierung bestätigen">Absenden</button>');
+      var checkbox = document.getElementsByClassName('storinerenBtn');
+      $('#grid-unten').append('<button  id="conformStorno" type="submit" value="Stornierung bestätigen">Absenden</button>');
 
-        for (var i = 0; i < checkbox.length; i++) {
-            $('#' + checkbox[i].id).on('change', function () {
-                var id = $(this).attr('id');
+      for (var i = 0; i < checkbox.length; i++) {
+        $('#' + checkbox[i].id).on('change', function () {
+          var id = $(this).attr('id');
 
-                if ($(this).prop('checked')) {
-                    SlotsToCancle.push(id);
+          if ($(this).prop('checked')) {
+            SlotsToCancle.push(id);
 
-                } else {
-                    SlotsToCancle = SlotsToCancle.filter(function (item) {
-                        return item !== id;
-                    });
-                    console.log(SlotsToCancle);
-
-                }
-
+          } else {
+            SlotsToCancle = SlotsToCancle.filter(function (item) {
+              return item !== id;
             });
-
             console.log(SlotsToCancle);
-        }
 
-        $('#conformStorno').click(function () {
-            if (SlotsToCancle.length > 0) {
-                storniereBuchung(SlotsToCancle);
-            } else {
-                console.log("Es wurden keine Buchungen ausgewählt, die storniert werden sollen!");
-            }
+          }
+
         });
-    }).fail(function (xhr) {
-        console.log('error received');
-    });
-});
 
+        console.log(SlotsToCancle);
+      }
+
+      $('#conformStorno').click(function () {
+        if (SlotsToCancle.length > 0) {
+          storniereBuchung(SlotsToCancle);
+        } else {
+          console.log("Es wurden keine Buchungen ausgewählt, die storniert werden sollen!");
+        }
+      });
+    }).fail(function (xhr) {
+      console.log('error received');
+    });
+  });
+}
 
 //--------------------------------------------------------//
 // Diese Funktion soll die .svg Dateien der Grundrisse aus
 // der Datenbank holen und anzeigen.
-function zeigeGebaeude(arr) {
+  function zeigeGebaeude(arr) {
     loadLageplan()
     let tmp;
 
     if (arr.length === 0) {
-        tmp.text('Keine Gebäude vorhanden');
-        return;
+      tmp.text('Keine Gebäude vorhanden');
+      return;
     }
 
     tmp = '<ul class="list-group" id="gebBtn">';
@@ -211,13 +217,12 @@ function zeigeGebaeude(arr) {
     console.log("log test");
     var i = 1;
     arr.forEach(obj => {
-        tmp += '<li id="GebBtn' + obj.id + '" class="list-group btn' + obj.id + '">' + obj.id + '</li>';
-        console.log('<li id="GebBtn' + obj.Name + '" class="list-group btn' + obj.Name + '">' + obj.Name + '</li>');
+      tmp += '<li id="GebBtn' + obj.id + '" class="list-group btn' + obj.id + '">' + obj.id + '</li>';
+      console.log('<li id="GebBtn' + obj.Name + '" class="list-group btn' + obj.Name + '">' + obj.Name + '</li>');
     });
     tmp += '</ul>';
     $('#sidebar').append(tmp);
 }
-
 
 function ladeGrundrisse(arr) {
     // IDs der Debäude Buttons in LiLds speichern
@@ -371,9 +376,9 @@ function storniereBuchung(ids) {
 
 
 
-
-$('.impressumBtn').click(function () {
+function impressum(){
     $('#grid-unten').empty();
+    $('#loginArea').empty();
 
     // disable default event
     event.preventDefault();
@@ -413,12 +418,14 @@ $('.impressumBtn').click(function () {
     $('#grid-unten').append('<div></div>');
     $('#grid-unten').append(meinString);
 
+    $('#loginArea').append('<div></div>');
+    $('#loginArea').append(meinString);
 
-});
+}
 
-$('.aboutBtn').click(function () {
+function aboutUs(){
     $('#grid-unten').empty();
-
+    $('#loginArea').empty();
     // disable default event
     event.preventDefault();
 
@@ -442,9 +449,9 @@ $('.aboutBtn').click(function () {
 
 
     $('#grid-unten').append(meinString);
+    $('#loginArea').append(meinString);
 
-
-});
+}
 
 
 
--- ReserveIT_Website/Frontend/js/main.js
+++ ReserveIT_Website/Frontend/js/main.js
@@ -7,8 +7,6 @@ function loginKiller(){
   document.getElementById("loginArea").id = "grid-unten";
   document.getElementById("logo").addEventListener("click", runMain);
   document.getElementById("lageplanBtn").addEventListener("click", runMain);
-  console.log(document.getElementById("logo"));
-  console.log(document.getElementById("lageplan"));
 }
 
 function runMain(){
@@ -106,4 +104,57 @@ function loadCalendar(){
   document.getElementById("calenderHolder").appendChild(calendar);
 
   const myCalendar = new WeeklyCalendar();
+}
+
+function loginBtn(){
+  document.getElementById("loginArea").innerHTML = '<div class="grid">\n' +
+    '\n' +
+    '      <form id="signInBtn" class="form login">\n' +
+    '\n' +
+    '        <div class="form__field">\n' +
+    '          <label for="login__matrikelnr"><svg class="icon">\n' +
+    '            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>\n' +
+    '          </svg><span class="hidden">matrikelnr</span></label>\n' +
+    '          <input id="login__matrikelnr" type="text" name="matrikelnr" class="form__input" placeholder="Matrikelnummer"\n' +
+    '                 pattern="[0-9]{1,5}" title="Please enter only numbers with a maximum length of 5 digits" required>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="form__field">\n' +
+    '          <label for="login__password"><svg class="icon">\n' +
+    '            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>\n' +
+    '          </svg><span class="hidden">Password</span></label>\n' +
+    '          <input id="login__password" type="password" name="password" class="form__input" placeholder="Password"\n' +
+    '                 required>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="form__field">\n' +
+    '          <input type="submit" value="Sign In">\n' +
+    '        </div>\n' +
+    '\n' +
+    '      </form>\n' +
+    '\n' +
+    '      <h1 id="fehler" style="color: red;"></h1>\n' +
+    '\n' +
+    '\n' +
+    '    </div>\n' +
+    '\n' +
+    '\n' +
+    '    <svg xmlns="http://www.w3.org/2000/svg" class="icons">\n' +
+    '      <symbol id="arrow-right" viewBox="0 0 1792 1792">\n' +
+    '        <path\n' +
+    '          d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z" />\n' +
+    '      </symbol>\n' +
+    '      <symbol id="lock" viewBox="0 0 1792 1792">\n' +
+    '        <path\n' +
+    '          d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z" />\n' +
+    '      </symbol>\n' +
+    '      <symbol id="user" viewBox="0 0 1792 1792">\n' +
+    '        <path\n' +
+    '          d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z" />\n' +
+    '      </symbol>\n' +
+    '    </svg>'
+    loginProcess();
+}
+function signedInLinkBar() {
+  document.getElementById("linkleiste").innerHTML = '<button id="lageplanBtn" class="btn lageplanBtn" type="button">Lageplan</button><button class="btn aboutBtn" id="ueberunsBtn" type="button" onclick="aboutUs()">Über uns</button> <button id="meineBuchungenBtn" class="btn buchungenBtn" type="button" onclick="">Meine Buchungen</button><button class="btn" id="impressumBtn" type="button" onclick="impressum()">Impressum</button>';
 }
